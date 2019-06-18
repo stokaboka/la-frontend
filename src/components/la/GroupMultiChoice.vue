f9639fe609bae6b7ff442ddd6aa62b5b527f9618
stokaboka <igothorev@gmail.com>
2019-06-18 13:35:36 +0300

    fix: set saved volume level

--- src/components/la/GroupMultiChoice.vue
+++ src/components/la/GroupMultiChoice.vue
@@ -63,6 +63,7 @@ export default {
 
     audio = new AudioHelper(this.api, 'assets/sounds')
     audio.init(this.api, '/assets/sounds').theme('')
+    audio.volume(this.volume)
     audio.on('START', this.onAudioFired)
       .on('PROGRESS', this.onAudioFired)
       .on('COMPLETE', this.onAudioFired)
