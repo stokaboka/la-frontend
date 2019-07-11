2bfbacbb2ddd8fa890bce35f117ef32361812f70
stokaboka <igothorev@gmail.com>
2019-07-11 13:33:24 +0300

    refactor: about form - text layout, colors

--- src/components/LaAbout.vue
+++ src/components/LaAbout.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="q-mt-lg text-h5 text-grey-14">
     <p>
-      <strong>Language Assessment ©</strong> - уникальная эффективная
+      <span class="la-about__text">Language Assessment ©</span> - уникальная эффективная
       система оценки знаний английского языка и отслеживания прогресса
-      студента, изучающего язык. Разработана методистами языкового
-      тренингового центра <q>Свобода слова</q> на базе международных
-      стандартов.
-    </p>
+      студента, изучающего язык.</p>
+      <p>Разработана методистами языкового
+      тренингового центра <strong><q>Свобода слова</q></strong> на базе международных
+      стандартов.</p>
   </div>
 </template>
 
@@ -19,5 +19,9 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  .la-about__text {
+    color: #027be3;
+    font-weight: 700;
+  }
 </style>
